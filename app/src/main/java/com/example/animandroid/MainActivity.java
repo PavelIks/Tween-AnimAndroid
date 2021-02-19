dfb3380fc598e0e61bcbf39d75b13b0986ede29b
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-02-19 23:00:10 +0200

    Updated-2.

--- app/src/main/java/com/example/animandroid/MainActivity.java
+++ app/src/main/java/com/example/animandroid/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.animandroid;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setBackgroundResource(R.drawable.a1);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.common_animation);
+        img.startAnimation(animation);
     }
 }
